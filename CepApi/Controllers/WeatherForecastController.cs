daee991d05b5a6ccaf187908b9fce2505f9cf040
sandro <saandrodiass369@gmail.com>
2022-05-06 19:40:14 -0300

    .

--- CepApi/Controllers/WeatherForecastController.cs
+++ CepApi/Controllers/WeatherForecastController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Net.Http.Json;
 namespace CepApi.Controllers
 {
     [ApiController]
@@ -30,31 +31,22 @@ namespace CepApi.Controllers
         [HttpGet]
         public endereco Get(string cep)
         {
-            var Listadeendereco = new List<endereco>
-            {
-               new endereco{Cep = "23560863" ,Bairro = "Santacruz", Estado = "Rj", Logradouro = "GUNADU2", TipodeLogradouro = "Rua" },
-               new endereco{Cep = "23658485" ,Bairro = "Caompo Grande", Estado = "Rj", Logradouro = "Rio da prata", TipodeLogradouro = "Rua" },
-               new endereco{Cep = "54748455" ,Bairro = "sepetiba", Estado = "Rj", Logradouro = "estrada da caixa ", TipodeLogradouro = "vloco" },
-               new endereco{Cep = "23560862" ,Bairro = "Santacruz", Estado = "Rj", Logradouro = "GUNADU2", TipodeLogradouro = "Rua" },
-               new endereco{Cep = "23560863" ,Bairro = "tatuap", Estado = "SP", Logradouro = "Beira Rio", TipodeLogradouro = "Rua" }
-             
 
-            };
-                                                      // execulta o primeiro que ver na lista se não existir não retorna nada  
-            endereco enderecos = Listadeendereco.Where(x => x.Cep == cep).FirstOrDefault();
+            RestClient client = new RestClient( string.Format ( $"https://viacep.com.br/ws/{cep}/json/"));
+            RestRequest request = new RestRequest("", Method.Get);
+            var Response = client.ExecuteGetAsync(request);
 
 
-                return (enderecos);
-         
-            
-            
 
-                
-            
-                
+
+            return ;
+
+
+
+
 
 
         }
-        
+       
     }
 }
--- CepApi/WeatherForecast.cs
+++ CepApi/WeatherForecast.cs
@@ -5,14 +5,18 @@ namespace CepApi
 {
     public class endereco
     {
-        public string Bairro { get; set; }
+      
 
         public string Cep { get; set; }
-
-        public string Estado { get; set; }
-
         public string Logradouro { get; set; }
-        public string TipodeLogradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+        public string Ddd { get; set; }
+        public string Siafi { get; set; }
+
+
     }
 
   
